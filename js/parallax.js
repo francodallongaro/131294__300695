5844c7bde3f686a4f09b387275681f3eae3a6dd3
Franco Dall'Ongaro <francodallongaro@gmail.com>
2021-09-21 11:15:27 -0300

    z-index fix

--- js/parallax.js
+++ js/parallax.js
@@ -10,5 +10,5 @@ window.addEventListener("scroll", function () {
   fran_photo.style.bottom = value * 0.5 + "px";
   who_i_am__p.style.top = value * 0.3 + 70 + "px";
 
-  my_values__title.style.top = (value + vh) * 0.1 + "px";
+  my_values__title.style.top = value * 0.4 + (vh - 50) + "px";
 });
