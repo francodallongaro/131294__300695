02a03ad4625a2e18f3bd892118fea254af6139a6
Franco Dall'Ongaro <francodallongaro@gmail.com>
2021-09-21 00:54:35 -0300

    styling parallax 2

--- js/parallax.js
+++ js/parallax.js
@@ -1,12 +1,14 @@
 let fran_photo = document.getElementById("who-i-am__img");
 let who_i_am__p = document.getElementById("who-i-am__p");
+let my_values__title = document.getElementById("my_values__title");
+
+let vh = window.innerHeight;
 
 window.addEventListener("scroll", function () {
   var value = window.scrollY;
 
   fran_photo.style.bottom = value * 0.5 + "px";
   who_i_am__p.style.top = value * 0.3 + 70 + "px";
-  if (value == 0) {
-    who_i_am__p.style.top = 70 + "px";
-  }
+
+  my_values__title.style.top = (value + vh) * 0.1 + "px";
 });
