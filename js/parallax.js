ee550e3f588947a8f7b7e312483f4e0800d294f2
Franco Dall'Ongaro <francodallongaro@gmail.com>
2021-09-21 00:39:16 -0300

    first parallax welcome

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,12 @@
-let fran_photo = document.getElementById("who-i-am_img");
+let fran_photo = document.getElementById("who-i-am__img");
+let who_i_am__p = document.getElementById("who-i-am__p");
 
 window.addEventListener("scroll", function () {
   var value = window.scrollY;
 
   fran_photo.style.bottom = value * 0.5 + "px";
+  who_i_am__p.style.top = value * 0.3 + 70 + "px";
+  if (value == 0) {
+    who_i_am__p.style.top = 70 + "px";
+  }
 });
