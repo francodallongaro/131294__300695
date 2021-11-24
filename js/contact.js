c8e67bc9fb2fdf8e3fffc5cace6d8847eb1d100d
Franco Dall'Ongaro <francodallongaro@gmail.com>
2021-11-24 09:11:02 -0300

    navbar color and meta tags

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,11 @@
 $("#contact-btn").click(function () {
   $("#contact-page").show();
+  $('.meta').remove();
+  $('head').append('<meta name="theme-color" class="meta" content="#d5e5e4"></meta>');
 });
 
 $("#contact-close").click(function () {
   $("#contact-page").hide();
+  $('.meta').remove();
+  $('head').append('<meta name="theme-color" class="meta" content="#f8f8f8"></meta>');
 });
